3b3744972839cc5bcc2b60a976011e713ea2afb4
Juan Colilla <juan.colilla@icloud.com>
2023-12-17 23:26:49 +0100

    Add swift-lint rules file.

--- BusRouter/SceneDelegate.swift
+++ BusRouter/SceneDelegate.swift
@@ -5,26 +5,25 @@
 //  Created by Juan Colilla on 17/12/23.
 //
 
-import UIKit
 import SwiftUI
+import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    
-    var window: UIWindow?
-    
-    func scene(
-        _ scene: UIScene,
-        willConnectTo session: UISceneSession,
-        options connectionOptions: UIScene.ConnectionOptions
-    ) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        let mainView = MapView()
-        
-        let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = UIHostingController(rootView: mainView)
-        
-        self.window = window
-        window.makeKeyAndVisible()
-    }
-}
 
+  var window: UIWindow?
+
+  func scene(
+    _ scene: UIScene,
+    willConnectTo session: UISceneSession,
+    options connectionOptions: UIScene.ConnectionOptions
+  ) {
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    let mainView = MapView()
+
+    let window = UIWindow(windowScene: windowScene)
+    window.rootViewController = UIHostingController(rootView: mainView)
+
+    self.window = window
+    window.makeKeyAndVisible()
+  }
+}
