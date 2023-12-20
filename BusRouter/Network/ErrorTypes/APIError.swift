2fee3e01bbe540a0e81b28dd555e981396c7d0f4
Juan Colilla <juan.colilla@icloud.com>
2023-12-20 21:28:08 +0100

    Network layer conscruction.

---
+++ BusRouter/Network/ErrorTypes/APIError.swift
@@ -0,0 +1,41 @@
+//
+//  APIError.swift
+//  BusRouter
+//
+//  Created by Juan Colilla on 20/12/23.
+//
+
+import Foundation
+
+enum APIError: LocalizedError {
+    // CODE
+    case BAD_URL
+    case MISSING_RESPONSE
+    
+    // INTERACTION
+    case CLIENT_ERROR(Int)
+    case SERVER_ERROR(Int)
+    case UNKNOWN_ERROR(Int)
+    case UNDERLYING_ERROR(Error)
+}
+
+extension APIError: Equatable {
+    static func == (lhs: APIError, rhs: APIError) -> Bool {
+        switch (lhs, rhs) {
+        case (.BAD_URL, .BAD_URL),
+            (.MISSING_RESPONSE, .MISSING_RESPONSE):
+            return true
+            
+        case let (.CLIENT_ERROR(lhsCode), .CLIENT_ERROR(rhsCode)),
+            let (.SERVER_ERROR(lhsCode), .SERVER_ERROR(rhsCode)),
+            let (.UNKNOWN_ERROR(lhsCode), .UNKNOWN_ERROR(rhsCode)):
+            return lhsCode == rhsCode
+            
+        case let (.UNDERLYING_ERROR(lhsError), .UNDERLYING_ERROR(rhsError)):
+            return lhsError.localizedDescription == rhsError.localizedDescription
+            
+        default:
+            return false
+        }
+    }
+}
