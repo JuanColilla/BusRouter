ac4f66aa07ecdb95c2770ae972f44b3307e15d86
Juan Colilla <juan.colilla@icloud.com>
2023-12-20 21:28:22 +0100

    Environment construction (only live).

---
+++ BusRouter/Environment/Environment.swift
@@ -0,0 +1,33 @@
+//
+//  Environment.swift
+//  BusRouter
+//
+//  Created by Juan Colilla on 20/12/23.
+//
+
+import Foundation
+import os.log
+
+var Current: Environment = Live()
+
+protocol Environment {
+    var apiClient: APIClient { get set }
+    var logger: Logger { get set }
+}
+
+extension Environment {
+    var languageCode: String {
+        Locale.current.languageCode!
+    }
+    var regionCode: String {
+        Locale.current.regionCode!
+    }
+    
+    var appVersion: String {
+        Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "N/A"
+    }
+    
+    var compilationNumber: String {
+        Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "N/A"
+    }
+}
